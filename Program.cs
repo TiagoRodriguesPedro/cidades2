75cdc44d1af8881e6542001f030162c82757782c
Tiago Rodrigues <isatiago098@gmail.com>
2021-08-29 04:47:27 -0300

    Resolução exerçicio cidades2

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace cidades2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cidades Litoral Sul de São Paulo");
+         Console.WriteLine("Santos");
+         Console.WriteLine("São Vicente");
+          Console.WriteLine("Praia Grande");
+             Console.WriteLine("Guáruja");
+            Console.WriteLine("Cubatão");
+           Console.WriteLine("Caraquatatuba");
+          Console.WriteLine("Intahaém");
+           Console.WriteLine("Ubatuba");
+            Console.WriteLine("São Sebastião");
+             Console.WriteLine("Peruibe");
+               Console.WriteLine("Bertioga");
+               Console.WriteLine("SMongaguá");
+              Console.WriteLine("Ilhabela");
+              Console.WriteLine("Iguápe");
+              Console.WriteLine("Cananéia");
+               Console.WriteLine("Ilha Comprida");
+        }
+    }
+}
